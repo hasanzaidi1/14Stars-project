8b4d1c9efe882402c1176cc4c1d428c8b277112e
hasanzaidi1 <hasanammarza@usf.edu>
2024-10-07 23:37:42 -0400

    remove func

--- app.js
+++ app.js
@@ -8,23 +8,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Route to handle student registration
 app.post('/register', (req, res) => {
-    const { fname, lname, age, grade, gender, parent_name_f, parent_name_l, parent_contact } = req.body;
+    const { 
+        fname, 
+        lname, 
+        age, 
+        grade, 
+        gender, 
+        parent_name_f, 
+        parent_name_l, 
+        parent_contact 
+    } = req.body;
 
-    // parent_contact will be an array if more than one is added
+    // Log the student information
     console.log(`
         Student First Name: ${fname}, 
         Student Last Name: ${lname}, 
         Age: ${age}, 
         Grade: ${grade}, 
         Gender: ${gender}, 
-        Parent First Name: ${parent_name_f}, 
-        Parent Last Name: ${parent_name_l},
-        Parent Contacts: ${parent_contact}`);
+        Parent First Names: ${parent_name_f.join(", ")}, 
+        Parent Last Names: ${parent_name_l.join(", ")},
+        Parent Contacts: ${parent_contact.join(", ")}`);
 
     // Save the student data, including multiple parent contacts, to the database
     res.json({ message: 'Student registered successfully!' });
 });
-
 
 // Serve static files like HTML/CSS/JS
 app.use(express.static('public'));
